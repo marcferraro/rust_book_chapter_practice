c9ccaea814d08a14ec149d3ec778b2d02ba794b6
Marc Ferraro <marc.ferraro1@gmail.com>
2023-08-09 22:40:54 -0500

    C to f converter, formatting decimals

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,17 @@
 fn main() {
     let fahrenheit = 72.0;
-    println!("{fahrenheit} degrees fahrenheit is ~{} degrees celsius.", 
-        fahrenheit_to_celsius(fahrenheit).floor())
+    let celsius = 32.0;
+    println!("{fahrenheit} degrees fahrenheit is ~{:.1} degrees celsius.", 
+        fahrenheit_to_celsius(fahrenheit));
+    
+    println!("{celsius} degrees celsius is ~{:.1} degrees fahrenheit.", 
+        celsius_to_fahrenheit(celsius));
 }
 
 fn fahrenheit_to_celsius(fahrenheit: f64) -> f64 {
     (5.0/9.0) * (fahrenheit - 32.0)
+}
+
+fn celsius_to_fahrenheit(celsius: f64) -> f64 {
+    (9.0/5.0 * celsius)+ 32.0
 }
