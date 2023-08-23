466f2260af0ac30c6577f67ec424b6426786d136
Marc Ferraro <marc.ferraro1@gmail.com>
2023-08-23 17:11:52 -0500

    vector_median fn

--- src/common_collections_practice.rs
+++ src/common_collections_practice.rs
@@ -2,3 +2,24 @@
 // median (when sorted, the value in the middle position) and 
 // mode (the value that occurs most often; a hash map will be helpful here) of the list.
 
+pub fn vector_median(mut v: Vec<i32>) -> f64 {
+    v.sort_unstable();
+    let length = v.len();
+    
+    if let 0 = length % 2 {
+        (v[(length / 2) - 1] as f64 + v[length / 2] as f64) as f64 / 2.0
+    } else {
+        println!("odd");
+        1.0
+    }
+
+
+    // sort array by value
+    // check length of array
+        // if even, average middle two numbers
+        // if odd, reutrn middle num
+}
+
+pub fn vector_mode() -> i32 {
+    todo!()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
 mod common_concepts_practice;
 mod common_collections_practice;
-use crate::common_concepts_practice as ccpractice;
-
 
 fn main() {
     let fahrenheit = 72.0;
     let celsius = 32.0;
     println!("{fahrenheit} degrees fahrenheit is ~{:.1} degrees celsius.\n", 
-        ccpractice::fahrenheit_to_celsius(fahrenheit));
+        common_concepts_practice::fahrenheit_to_celsius(fahrenheit));
     
     println!("{celsius} degrees celsius is ~{:.1} degrees fahrenheit.\n", 
-        ccpractice::celsius_to_fahrenheit(celsius));
+        common_concepts_practice::celsius_to_fahrenheit(celsius));
     
     let fibonacci_target = 13;
     println!("Number {fibonacci_target} of the Fibonacci Sequence is {}.\n",
-        ccpractice::find_nth_fibonacci(fibonacci_target));
+        common_concepts_practice::find_nth_fibonacci(fibonacci_target));
     
-    ccpractice::print_twelve_days_of_christmas()
+    common_concepts_practice::print_twelve_days_of_christmas();
+
+    let unsorted_ints: Vec<i32> = vec![6, 3, 2, 7, 8, 9, 5, 10, 4, 1];
+    let median = common_collections_practice::vector_median(unsorted_ints);
+    println!("The median of the array {}.", median);
 }
